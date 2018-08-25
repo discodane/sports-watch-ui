828688fb28e77a97e705ec44df4b5cbe76710d06
Heidi Erickson <heidierickson@Heidis-MacBook-Air.local>
2018-08-25 16:10:13 -0500

    adds navigation arrows

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+import YouTube from 'react-youtube';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      somethingCray: [],
+      iterator: 0,
+    };
+    this.changeIterator = this.changeIterator.bind(this);
+  }
+  componentDidMount() {
+    axios.get("http://localhost:3000")
+      .then((response) => {
+        let i = 0;
+        for(; i < response.data.length; i++) {
+          const someList = [
+            ...this.state.somethingCray,
+            response.data[i].id
+          ]
+          this.setState({ somethingCray: someList });
+        }
+      })
+      .catch((err) => {
+        console.log("there was an error", err);
+      });
+  }
+  changeIterator(value) {
+    if(this.state.iterator === this.state.somethingCray.length - 1) return;
+    this.setState({iterator: this.state.iterator + value})
+  }
+
+
   render() {
+    const {somethingCray, iterator} = this.state;
+    const opts = {
+      playerVars: {
+        autoplay: 1
+      }
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FaAngleLeft size={"10em"} color={iterator === 0 ? 'grey' : 'inherit'} onClick={() => this.changeIterator(-1)} />
+        {somethingCray ? (
+            <YouTube
+              key={somethingCray[iterator]}
+              onEnd={() => {this.changeIterator(1)}}
+              opts={opts}
+              videoId={somethingCray[iterator]}
+            />
+          ) :
+          (<p>Loading...</p>)
+        }
+        <FaAngleRight size={"10em"} color={iterator === somethingCray.length - 1 ? 'grey' : 'inherit'} onClick={() => this.changeIterator(1)} />
       </div>
     );
   }
